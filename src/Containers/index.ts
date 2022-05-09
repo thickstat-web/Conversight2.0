0d3651ec96809b02ff3c12ad4ce10e1b7e1bb8c9
SAKTHIVEL MURUGASAMY <sakthivel.murugasamy@conversight.ai>
2022-05-09 13:56:53 +0530

    Removed Login and VerifyEmail Containers

--- src/Containers/index.ts
+++ src/Containers/index.ts
@@ -1,5 +1,3 @@
-export { default as VerifyEmailContainer } from './VerifyEmailContainer'
-export { default as LoginContainer } from './LoginContainer'
 export { default as StartupContainer } from './StartupContainer'
 export { default as ChatContainer } from './ChatContainer'
 export { default as EnterEmailContainer } from './EnterEmailContainer'
