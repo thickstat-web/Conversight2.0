6965bafc651acab821c6b0f59d94f9333cdb5b35
SAKTHIVEL MURUGASAMY <sakthivel.murugasamy@conversight.ai>
2022-06-20 12:29:57 +0530

    Ingress API base url added

--- src/Config/index.ts
+++ src/Config/index.ts
@@ -1,6 +1,7 @@
 export const Config = {
   CS_API_URL: 'https://api.conversight.ai',
   BOT_API_URL: 'https://bot.conversight.ai/v2',
+  INGRESS_API_URL: 'https://ingress.conversight.ai',
 }
 
 // Replace following credentials below for testing
