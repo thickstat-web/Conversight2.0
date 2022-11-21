a4dcfb82a9aa77b24b229beb400e10271e546425
SAKTHIVEL MURUGASAMY <sakthivel.murugasamy@conversight.ai>
2022-11-21 13:19:51 +0530

    React native voice and Vector fonts added for iOS

--- react-native.config.js
+++ react-native.config.js
@@ -4,4 +4,11 @@ module.exports = {
     android: {},
   },
   assets: ['./src/Assets/fonts'],
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null,
+      },
+    },
+  },
 }
