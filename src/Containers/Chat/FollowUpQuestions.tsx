eda86cf6dbf7bdf1c6e0c9c7e80067c659af552b
hariharasudhan.pandiyan <hariharasudhan.pandiyan@conversight.ai>
2023-03-03 13:24:56 +0530

    athenaicon is changed to static in follow up question

--- src/Containers/Chat/FollowUpQuestions.tsx
+++ src/Containers/Chat/FollowUpQuestions.tsx
@@ -36,16 +36,18 @@ function FollowUpQuestions({
         <Text style={{ color: Colors.GREEN_MAIN, fontWeight: 'bold' }}>
           Are you interested in,
         </Text>
-        <ScrollView
-          horizontal={true}
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={{ alignItems: 'center' }}
-        >
+        <View style={{ flexDirection: 'row' }}>
           <View style={styles.athenaIcon}>
             <Image source={AthenaIcon} />
           </View>
-          {renderedItems}
-        </ScrollView>
+          <ScrollView
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{ alignItems: 'center' }}
+          >
+            {renderedItems}
+          </ScrollView>
+        </View>
       </View>
     </View>
   )
