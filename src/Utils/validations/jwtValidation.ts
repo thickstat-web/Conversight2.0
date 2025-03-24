3e338510a5cc927230458eab14220387dfac1618
hariharasudhan <hariharasudhan.pandiyan@conversight.ai>
2025-03-24 16:05:02 +0530

    casdoor implementation

--- src/Utils/validations/jwtValidation.ts
+++ src/Utils/validations/jwtValidation.ts
@@ -1,12 +1,9 @@
-import { jwtDecode } from "jwt-decode";
-interface DecodedToken {
-    exp: number;
-    [key: string]: any;
-}
+import { Buffer } from 'buffer';
+
 
 export const validateJWT = (jwt: string): boolean => {
     try {
-        const decoded: DecodedToken = jwtDecode(jwt);
+        const decoded = JSON.parse(Buffer.from(jwt.split('.')[1], 'base64').toString())
         const currentTime = Math.floor(Date.now() / 1000);
         if (decoded.exp && decoded.exp < currentTime) {
             return false;
